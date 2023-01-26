ee91101aa713e3fcab8063315eb1765e9111defc
김의현 <dmlgus1922@naver.com>
2023-01-26 21:11:40 +0900

    React Native: 23.01.26 / 노마드코더
    섹션 2
    날씨 보여주는 어플 만들기 끝.
    1. 오픈웨더 사이트에서 API 받아서 날씨 가져오기
     - 지역은 expo-location 라이브러리 이용. 홈페이지에서 Docs보면서 필요한 거 쓰자.
    2. 가져온 json 파일 내에서 필요한 정보 찾아보기
    3. icons.expo.fyi 사이트에서 아이콘 쓰기

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,28 +1,55 @@
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+
+import { Fontisto } from '@expo/vector-icons';
+// expo init 하면서 알아서 생긴 라이브러리
+// https://icons.expo.fyi/ 아이콘 미리보기 사이트
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
+const API_KEY = '31c486db6d8794366cf38e61e8c7c203';
+
+const icons = {
+  Clouds: 'cloudy',
+  Clear: 'day-sunny',
+  Atmosphere: '',
+  Snow: 'snowflake',
+  Rain: 'rains',
+  Drizzle: 'rain',
+  Thunderstorm: 'lightning',
+};
+
 export default function App() {
-  const [location, setLocation] = useState();
   const [city, setCity] = useState('Loading..');
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
 
-  const ask = async() => {
+
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
-    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
     setCity(location[0].region);
+    // 공짜 키라서 데일리 웨더가 불가능
+    //const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    const json = await response.json();
+    // const temp = Math.round((json.main.temp) * 100) / 100;
+    const temp = parseFloat(json.main.temp).toFixed(1);
+    const weather = json.weather[0].main;
+    const description = json.weather[0].description;
+    setDays([temp, weather, description]);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
+  
   return (
     // View는 div같은 콘테이너. 항상 import해야 한다.
     <View style={styles.container}>
@@ -32,25 +59,46 @@ export default function App() {
       </View>
       <ScrollView
         horizontal
-        contentContainerStyle={styles.weather}
+        contentContainerStyle={styles.forecast}
         pagingEnabled
         showsHorizontalScrollIndicator={false}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
+        {
+          days.length === 0 ?
+            <View style={styles.loading}>
+              <ActivityIndicator size={'large'} color='black' />
+            </View>
+            :
+            <View style={styles.day}>
+              <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between', width: '85%' }}>
+                <Text style={styles.temp}>{days[0]}</Text>
+                <Fontisto name={icons[days[1]]} size={65} color="white" />
+              </View>
+
+              <Text style={styles.weather}>{days[1]}</Text>
+              <Text style={styles.description}>{days[2]}</Text>
+            </View>
+        }
+        {/* <View style={styles.day}>
+          {
+            days.length === 0
+              ?
+              'loading...'
+              :
+              <>
+                <Text style={styles.temp}>{days[0]}</Text>
+                <Text style={styles.description}>{days[1]}</Text>
+              </>
+          }
+        </View> */}
+        <View style={styles.loading}>
+          <ActivityIndicator size={'large'} color='black' />
         </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
+        <View style={styles.loading}>
+          <ActivityIndicator size={'large'} color='black' />
         </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
+        <View style={styles.loading}>
+          <ActivityIndicator size={'large'} color='black' />
         </View>
       </ScrollView>
       <StatusBar style='auto'></StatusBar>
@@ -72,21 +120,34 @@ const styles = StyleSheet.create({
   },
   cityName: {
     color: 'black',
-    fontSize: 60,
-    fontWeight: '800'
+    fontSize: 50,
+    fontWeight: '600'
   },
-  weather: {
+  forecast: {
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    marginLeft: SCREEN_WIDTH / 12,
+    marginRight: -SCREEN_WIDTH / 12
   },
   temp: {
+    color: 'teal',
     marginTop: 20,
-    fontSize: 178,
+    fontSize: 100,
+  },
+  weather: {
+    color: 'white',
+    fontSize: 40,
   },
   description: {
-    marginTop: -40,
-    fontSize: 60
-  }
+    color: 'black',
+    marginTop: -10,
+    fontSize: 20
+  },
+  loading: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+    paddingTop: 100
+  },
 });
